9b91019970b39a15212e9903d2727e7a94c605a2
caosisa <dlaehddus18@naver.com>
2024-07-05 15:47:10 +0900

    feat/#14 변수명 수정

--- accounts/models.py
+++ accounts/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class User(models.Model):
     user_id = models.AutoField(primary_key=True)
-    user_name = models.CharField(max_length=20)
+    email = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
     nickname = models.CharField(max_length=20)
     name = models.CharField(max_length=10)
-    create_at = models.DateField(auto_now_add=True)
-    update_at = models.DateField(auto_now=True)
-    delete_at = models.DateField(null=True)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    is_deleted = models.DateField(null=True)
