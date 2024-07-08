2b5ab89ecd56e7d06c8062401be995d4361666d3
Bobae Kim <rlaqhqo0418@naver.com>
2024-07-08 17:09:24 +0900

    feat: channel 모델 추가

--- djangoIteam/settings.py
+++ djangoIteam/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'classify_news',
     'news',
     'feedbacks',
+    'channels'
 ]
 
 MIDDLEWARE = [
--- news/models.py
+++ news/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from channels.models import Channel
 
 class News(models.Model):
     news_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=50)
     content = models.TextField()
     category = models.CharField(max_length=20)
-    channel_id = models.IntegerField()
+    channel_id = models.ForeignKey(Channel, related_name='channel_id', on_delete=models.DO_NOTHING)
     published_date = models.DateField()
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
