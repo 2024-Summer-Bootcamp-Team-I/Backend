a8cf638b7a38354501bf8a71b6b1b5540600a71a
Bobae Kim <rlaqhqo0418@naver.com>
2024-07-08 14:17:23 +0900

    remove: classify_news view 삭제

--- classify_news/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ClassifiesAPIView, ClassifyAPIView
-
-urlpatterns = [
-    path("", ClassifiesAPIView.as_view()),
-    path("<int:news_id>", ClassifyAPIView.as_view()),
-]
