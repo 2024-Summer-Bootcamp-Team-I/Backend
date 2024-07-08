3adf076377d07343f70a7b3f2a3afea2946c380e
Bobae Kim <rlaqhqo0418@naver.com>
2024-07-08 22:06:09 +0900

    feat: 변수명 변경

--- news/models.py
+++ news/models.py
@@ -3,11 +3,9 @@ from django.db import models
 class News(models.Model):
     news_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=50)
-    image = models.ImageField(null=True)
     content = models.TextField()
     category = models.CharField(max_length=20)
-    channel = models.CharField(max_length=40)
     published_date = models.DateField()
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
-    deleted_at = models.DateField(null=True)
+    is_deleted = models.BooleanField(default=False)
