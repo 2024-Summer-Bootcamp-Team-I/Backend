d432f6baed451db433deaa983c585ba8534825c7
Bobae Kim <rlaqhqo0418@naver.com>
2024-07-18 15:18:40 +0900

    feat: 뉴스기사 개수 조회 구현

--- news/serializers.py
+++ news/serializers.py
@@ -10,6 +10,12 @@ class news_data_Serializer(serializers.ModelSerializer):
 class news_score_Serializer(serializers.ModelSerializer):
     classifynews_score = ClassifyNews
 
+class news_count_Serializer(serializers.ModelSerializer):
+    news_count = serializers.IntegerField()
+    class Meta:
+        model = News
+        fields = ("news_count", "created_at")
+
 class correctrespones_Serializer(serializers.Serializer):
     message = serializers.CharField()
 
