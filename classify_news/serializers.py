95503a01e64f8a64fe92511e1fbc3a062953fcf3
Bobae Kim <rlaqhqo0418@naver.com>
2024-07-24 01:40:53 +0900

    feat: 판별뉴스 정보 채널이름 추가

--- classify_news/serializers.py
+++ classify_news/serializers.py
@@ -4,9 +4,13 @@ from news.serializers import news_data_Serializer
 
 class ClassifyNewsSerializer(serializers.ModelSerializer):
     news = news_data_Serializer(source='news_id', read_only=True)
+    channel_name = serializers.SerializerMethodField()
     class Meta:
         model = ClassifyNews
         fields = ("news", "score", "reason", "created_at","updated_at","is_deleted")
+
+    def get_channel_name(self, obj):
+        return obj.news_id.channel.name
 
 class ClassifyNewsCreateSerializer(serializers.ModelSerializer):
     class Meta:
