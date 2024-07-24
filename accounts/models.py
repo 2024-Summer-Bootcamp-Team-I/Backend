40f5ed5e44ba9d85a9d9ab3c3697e5cbb3729bff
Bobae Kim <rlaqhqo0418@naver.com>
2024-07-24 15:12:05 +0900

    feat: 회원 이메일 길이 증가

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class User(models.Model):
     user_id = models.AutoField(primary_key=True)
-    email = models.CharField(max_length=20)
+    email = models.CharField(max_length=100)
     password = models.CharField(max_length=20)
     name = models.CharField(max_length=10)
     created_at = models.DateField(auto_now_add=True)
