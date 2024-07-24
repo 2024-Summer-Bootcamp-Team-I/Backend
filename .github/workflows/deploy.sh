15710a90054aacb83bd7501b639003751c28e7b0
caosisa <dlaehddus18@naver.com>
2024-07-25 00:27:10 +0900

    fix: 아직 시도 안해봤지만 코드 이쁘게 짠듯 이번에 시도해본다.

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -8,16 +8,13 @@ cd /home/ubuntu/Backend || exit
 echo "Pulling latest changes from Git..."
 git pull origin main
 
-echo "Loading Docker image..."
-docker load -i fake_news.tar
+echo "Pulling the latest Docker image..."
+docker-compose pull
 
-echo "Stopping existing Docker container..."
-docker stop fake_news-container || true
+echo "Stopping and removing existing Docker containers..."
+docker-compose down
 
-echo "Removing existing Docker container..."
-docker rm fake_news-container || true
-
-echo "Running new Docker container..."
-docker run -d --name fake_news-container -p 80:80 fake_news:latest
+echo "Starting new Docker containers..."
+docker-compose up -d
 
 echo "Deployment complete!"
--- djangoIteam/settings.py
+++ djangoIteam/settings.py
@@ -90,11 +90,11 @@ WSGI_APPLICATION = "djangoIteam.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': '4414',
-        'HOST': 'db',
-        'PORT': '5432',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT'),
     }
 }
 
