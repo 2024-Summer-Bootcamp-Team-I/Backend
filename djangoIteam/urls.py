4fe4296c6f1d84e7ef458903d4023471526fdcd0
caosisa <dlaehddus18@naver.com>
2024-07-29 23:56:40 +0900

    fix:openapi오류

--- djangoIteam/urls.py
+++ djangoIteam/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, re_path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework.permissions import AllowAny
-
 schema_view = get_schema_view(
     openapi.Info(
         title="Fakenews",
@@ -11,22 +10,22 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=[AllowAny],
-    urlconf='‘https://fakenew.site/’',
+    url='https://fakenew.site',
 )
-
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/accounts/",include('accounts.urls')),
+    path("api/v1/accounts/", include('accounts.urls')),
     path("api/v1/channels/", include('channels.urls')),
-    path('api/v1/news/', include('news.urls')), 
-    path("api/v1/classifies/<int:news_id>/feedbacks/", include('feedbacks.urls')),                             
+    path('api/v1/news/', include('news.urls')),
+    path("api/v1/classifies/<int:news_id>/feedbacks/", include('feedbacks.urls')),
     path("api/v1/classifies/", include('classify_news.urls')),
     path("api/v1/scraps/", include('scraped_news.urls')),
     path('', include('django_prometheus.urls')),
-
-]
-urlpatterns += [
+    path("swagger/", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path("redoc/", schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+# Ensure Django uses HTTPS
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
