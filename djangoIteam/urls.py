087054f0ba900687abac3786861ec75eef0bf3a8
jihye9871 <jihye9871@naver.com>
2024-07-29 23:46:54 +0900

    hotfix: https swagger 설정 변경

--- djangoIteam/urls.py
+++ djangoIteam/urls.py
@@ -15,6 +15,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path("swagger/", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path("admin/", admin.site.urls),
     path("api/v1/accounts/",include('accounts.urls')),
     path("api/v1/channels/", include('channels.urls')),
